0140004fe09cb6cf6344a812e88bb99f868a6bc6
Mariana Vasconcelos <mavapisi16@icloud.com>
2025-06-20 20:38:38 +0100

    Main view window adjustment

--- controllers/app_controller.py
+++ controllers/app_controller.py
@@ -4,10 +4,8 @@ from views.main_view import MainFrame
 class AppController(ctk.CTk):
     def __init__(self):
         super().__init__()
-        self.title('Time Manager')
-        self.configure(fg_color='#2d804f')
-        self.state("zoomed")
-
+        self.title('Gestor de Finanças')
+        self.after(0, lambda: self.state("zoomed"))
         self.frames = {}
         self.build_frames()
         self.show_frame("main")
@@ -16,4 +14,19 @@ class AppController(ctk.CTk):
         self.frames["main"] = MainFrame(self, self)
 
         for frame in self.frames.values():
+            frame.place(relwidth=1, relheight=1)
+
+
+    def show_frame(self, name, **kwargs):
+        for frame in self.frames.values():
             frame.place_forget()
+
+        frame = self.frames[name]
+        frame.place(relwidth=1, relheight=1)
+        frame.tkraise()
+
+        if name == "timer" and "project" in kwargs:
+            frame.start(kwargs["project"])
+
+        if hasattr(frame, "on_show"):
+            frame.on_show()
--- views/main_view.py
+++ views/main_view.py
@@ -1,21 +1,18 @@
 import customtkinter as ctk
-import os
-import utils.styles
 
 class MainFrame(ctk.CTkFrame):
     def __init__(self, parent, controller):
         super().__init__(parent)
-        self.place(relwidth=1, relheight=1)
-        self.pack_propagate(False)
-        self.main_frame = MainFrame(self, controller=None)
-        self.main_frame.pack(fill="both", expand=True)
+        self.controller = controller
+        self.configure(fg_color='#1d6b43')
         self.build_ui()
 
     def build_ui(self):
-        title_label = ctk.CTkLabel(self, text="Finance Manager", font=ctk.CTkFont(size=24, weight="bold"))
+        font_title = ctk.CTkFont("Arial Rounded MT Bold", size=40, weight="bold")
 
-
-
-
-
-
+        ctk.CTkLabel(
+            self,
+            text="Gestor de Finanças",
+            font=font_title,
+            text_color="#ffffff"
+        ).pack(pady=(20, 10))
